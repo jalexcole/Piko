d5bd1f7e78f2a2efb7211c888d58e3fa35644a98
alex cole <jalexcole@gmail.com>
2021-09-13 21:54:36 -0500

    fixes and cleanup

---
+++ lib/src/main/java/com/piko/SimpleApplication.java
@@ -0,0 +1,14 @@
+package com.piko;
+
+import com.piko.engine.*;
+import com.piko.engine.SwingEngine;
+
+public abstract class SimpleApplication {
+
+
+    public abstract void init();
+    public abstract void update();
+    public abstract void draw();
+
+    
+}
