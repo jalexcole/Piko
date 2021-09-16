350c0f3ab0fee4cb13dbae0a29d21a3e4eb4a4b7
alex cole <jalexcole@gmail.com>
2021-09-16 12:17:02 -0500

    Worked on sprites

--- lib/src/main/java/com/piko/util/Sprite.java
+++ lib/src/main/java/com/piko/util/Sprite.java
@@ -1,18 +1,55 @@
 package com.piko.util;
 
 import java.awt.Graphics;
+import java.awt.Image;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import com.piko.pipeline.Renderable;
 
 public class Sprite implements Renderable{
-    
-    Sprite(String filepath) {
+    Image image;
+    int x;
+    int y;
 
+    Sprite(String filepath) {
+        loadImage(filepath);
+    }
+
+    private void loadImage(String filepath) {
+        if (isImageFileType(filepath)) {
+            try {
+                image = ImageIO.read(new File(filepath));
+             } catch (IOException e) {
+                e.printStackTrace();
+             }
+        }
+    }
+
+    private boolean isImageFileType(String filepath) {
+        String[] file = filepath.split(".");
+        
+        switch (file[file.length - 1]) {
+            case "png" :
+                return true;
+            case "jpeg" :
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     @Override
     public void draw(Graphics graphics) {
-        // graphics.drawImage(arg0, arg1, arg2, arg3)
+        graphics.drawImage(image, x, y, null);
         
     }
     
